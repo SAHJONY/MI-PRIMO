b35d8fa6d13611c07203f8c3dfe95c72ee39b197
root <root@srv767849.hstgr.cloud>
2026-05-09 23:57:39 +0000

    Add logo

---
+++ frontdesk-agents/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MI PRIMO</h1>
+        <img src="https://via.placeholder.com/150" alt="Logo" className="App-logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
